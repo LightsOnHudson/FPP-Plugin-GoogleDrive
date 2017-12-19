002fdd256e338b2de417a85e0741f4c39f04ef51
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-12-19 08:37:19 -0700

    Update

--- examples/simple-file-upload.php
+++ examples/simple-file-upload.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-include_once __DIR__ . '/../src/Google/autoload.php';
+include_once '/home/fpp/media/plugins/GoogleDrive/src/Google/autoload.php';
 include_once "templates/base.php";
 
 echo pageHeader("File Upload - Uploading a simple file");
